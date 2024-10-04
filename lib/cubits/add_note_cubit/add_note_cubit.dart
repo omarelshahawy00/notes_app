4d77c9748806625af3ccfc0de25138eb863bb59e
Omar Elshahawy <omar.elshahawy00@gmail.com>
2024-10-04 15:05:06 +0300

    add note state

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -3,6 +3,6 @@ import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
 
-class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
-  AddNoteCubitCubit() : super(AddNoteCubitInitial());
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
 }
